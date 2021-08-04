64bbdd5f688ffc696fd58f086e3771b14b97098a
medquota <87965303+medquota@users.noreply.github.com>
2021-08-04 12:41:46 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,33 @@
-// module.exports = {
-//     //
-//     graphql: {
-//       endpoint: '/graphql',
-//       tracing: true,
-//       shadowCRUD: true,
-//       playgroundAlways: false,
-//       depthLimit: 7,
-//       amountLimit: 50,
-//     },
-//   };
-
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    return {
-      upload: {
-        provider: "aws-s3",
-        providerOptions: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET"),
-          },
-        },
-      },
-    };
-  }
-
-  return {
-    //Empty config which will use defaults
+module.exports = {
+    //
+    graphql: {
+      endpoint: '/graphql',
+      tracing: true,
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 50,
+    },
   };
-};
+
+// module.exports = ({ env }) => {
+//   if (env("NODE_ENV") === "production") {
+//     return {
+//       upload: {
+//         provider: "aws-s3",
+//         providerOptions: {
+//           accessKeyId: env("AWS_ACCESS_KEY_ID"),
+//           secretAccessKey: env("AWS_ACCESS_SECRET"),
+//           region: env("AWS_REGION"),
+//           params: {
+//             Bucket: env("AWS_BUCKET"),
+//           },
+//         },
+//       },
+//     };
+//   }
+
+//   return {
+//     //Empty config which will use defaults
+//   };
+// };
